f7de5005c18a6f9f47e71c8640bb22e0e89bf63b
sharoonthomas1 <159278757+sharoonthomas1@users.noreply.github.com>
2024-08-26 20:20:22 +0530

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+// $(document).ready(function() {
+//     $(window).scroll(function() {
+//         var scroll = $(window).scrollTop();
+//         if (scroll > 100) {
+//             $(".black").css("background", "green");
+//         } else {
+//             $(".black").css("background", " rgba(255, 255, 255, 0.6)");
+//         }
+//     });
+// });
+  
